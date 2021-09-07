890300163ad7e044afdad8cfb2a53cccdb205ab3
Young Jun Do <youngjundoe@gmail.com>
2021-09-07 17:38:45 -0400

    commit tree b07e30f0813a453a58b8bdcfb731f53177972786

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+} 
+
+function wrapAdjective(flair='*'){
+    return function(word = "special"){
+        return `You are ${flair}${word}${flair}!`;
+    }
+}
